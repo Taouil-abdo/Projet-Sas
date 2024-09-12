e80e4f21da8f22f2234e479f87ac275cb8015ea8
Taouil-abdo <abdotaouil03@gmail.com>
2024-09-12 17:49:01 +0200

    ajouter des fonctionalite et fix des bug

--- main.c
+++ main.c
@@ -3,12 +3,9 @@
 #include <string.h>
 
 
-
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
-
 struct etudiant
 {
-	int id;
+    int id;
     char Nom[50];
     char Prenom[50];
     char DateNaissance[50];
@@ -20,6 +17,7 @@ struct etudiant et[50];
 int totalEtudiants = 0;
 int i ,j,k;
 
+//========================= Partie AjouterEtudiants
 
 void Ajouter()
 {
@@ -33,7 +31,7 @@ void Ajouter()
 	scanf("%s",et[totalEtudiants].Prenom);
 	printf("Enter la Date de naissance : \n");
 	scanf("%s",et[totalEtudiants].DateNaissance);
-	printf("Enter le Departement : \n");
+	printf("Enter le Departement :[Math , info , pc , svt] \n");
 	scanf("%s",et[totalEtudiants].Departement);
 	printf("Enter la Note generale : \n");
 	scanf("%f",&et[totalEtudiants].Notegenerale);
@@ -42,6 +40,8 @@ void Ajouter()
 	printf("Bien Ajouter ! \n");
 
 }
+
+//=========================Partie AfficherEtudiants
 
 
 void Afficher()
@@ -58,7 +58,10 @@ void Afficher()
 	}
 }
 
-void Rechercher()
+//=========================Partie Rechercher
+
+//1 .Rechercher Par Nom
+void RechercherParNom()
 {
    char RecherchNom[50];
    int found = 0;
@@ -69,7 +72,6 @@ void Rechercher()
       	for(i = 0 ; i < totalEtudiants ; i++)
    {
    	   if(strcmp(RecherchNom,et[i].Nom) == 0)
-
    	{
    		printf("votre id est %d\n",et[i].id);
 		printf("le Nom est : %s\n",et[i].Nom);
@@ -78,11 +80,61 @@ void Rechercher()
 		printf("le Departement : %s\n",et[i].Departement);
 		printf("la Note generale : %.2f\n",et[i].Notegenerale);
 	   }
-    
    }
-
 }
 
+//2. AfficherEtudiantsParDepartement
+
+void AfficherEtudiantsParDepartement()
+{
+   char RecherchDepartement[50];
+   
+   printf("enter le Departement d'etudiant que vous avez de recherche :\n");
+   scanf("%s",RecherchDepartement);
+   
+      	for(i = 0 ; i < totalEtudiants ; i++)
+   {
+   	   if(strcmp(RecherchDepartement,et[i].Departement) == 0)
+   	{
+   		printf("votre id est %d\n",et[i].id);
+		printf("le Nom est : %s\n",et[i].Nom);
+		printf("le Prenom : %s\n",et[i].Prenom);
+		printf("la Date de Naissance : %s\n",et[i].DateNaissance);
+		printf("le Departement : %s\n",et[i].Departement);
+		printf("la Note generale : %.2f\n\n",et[i].Notegenerale);
+	   }
+   }
+}
+
+void Rechercher()
+{
+	int choix3 ;
+	
+		do{
+			
+		printf("1.Rechercher Par Nom.\n");
+		printf("2.Afficher Etudiants Par Departement.\n");
+		printf("3.Quitte.\n");
+        printf("que vous choisir : \n");
+        scanf("%d",&choix3);
+        
+        switch(choix3)
+        {
+        case 1:
+             RechercherParNom();
+            break;
+        case 2:
+            AfficherEtudiantsParDepartement();
+            break;
+        case 3:
+        	return;
+            break;
+        default:
+        	break;
+        }
+}while(choix3 != 4);
+}
+//=========================Partie ModifierEtudiant
 
 void Modifier()
 {
@@ -119,22 +171,23 @@ void Modifier()
     }
 }
 
+//=========================Partie supprimerEtudiant
+
 
 void supprimer()
 {
 	int Recherchid;
 
-    printf("Entrer l'id de l'etudiant que vous voulez modifier : \n");
+    printf("Entrer l'id de l'etudiant que vous voulez supprimer : \n");
     scanf("%d", &Recherchid);
     
     for(i = 0 ; i < totalEtudiants ; i++)
     {
     	if(Recherchid == et[i].id)
     	{
-    		
     		while(i < totalEtudiants)
     		{
-    			 et[i].id = et[i+1].id ;
+    			et[i].id = et[i+1].id ;
     			strcpy(et[i].Nom, et[i+1].Nom);
     			strcpy(et[i].Prenom, et[i+1].Prenom);
     			strcpy(et[i].Departement, et[i+1].Departement);
@@ -143,20 +196,21 @@ void supprimer()
     			
             totalEtudiants--;
             i++;
-            printf("Étudiant Bien supprimé \n");
+            printf("Etudiant Bien supprime \n");
 
 			}
 		}
-		printf("Etudaint avec l'id %d non trouve. : \n");
-	}
-	
+	}		printf("Etudaint avec l'id %d non trouve. : \n");
 }
 
+//=========================Partie calculerMoyenneGenerale
 
 void calculerMoyenneGenerale() {
 	
     float somme_totale_universite = 0;
     int nombre_total_etudiants = 0;
+    float somme_notes_departement = 0;
+    int nombre_etudiants_departement = 0;
 
     for (i = 0; i < totalEtudiants; i++) {
         char departementCourant[50];
@@ -171,9 +225,7 @@ void calculerMoyenneGenerale() {
         }
         
         if (!deja_calculer) {
-            float somme_notes_departement = 0;
-            int nombre_etudiants_departement = 0;
-            
+        
             for (k = 0; k < totalEtudiants; k++) {
                 if (strcmp(et[k].Departement, departementCourant) == 0) {
                     somme_notes_departement += et[k].Notegenerale;
@@ -199,7 +251,9 @@ void calculerMoyenneGenerale() {
     }
 }
 
+//========================= Partie Statistique
 
+ 
 // 1. Afficher le nombre total d'étudiants inscrits
 
 void afficherNombreTotalEtudiants() {
@@ -209,25 +263,110 @@ void afficherNombreTotalEtudiants() {
 // 2. Afficher le nombre d'étudiants dans chaque département
 
 void NomberEtudParDepartement()
-{	
-	
-	
+{
+    char departements[50][50];
+    int compteurs[50]={0};
+    int nombre_departements = 0;
+
+    for (i = 0; i < totalEtudiants; i++)
+    {
+        int deja_ajoute = 0;
+        for (j = 0; j < nombre_departements; j++)
+        {
+            if (strcmp(et[i].Departement, departements[j]) == 0)
+            {
+                compteurs[j]++;
+                deja_ajoute = 1;
+                break;
+            }
+        }
+
+        if (!deja_ajoute)
+        {
+            strcpy(departements[nombre_departements], et[i].Departement);
+            compteurs[nombre_departements]++;
+            nombre_departements++;
+        }
+    }
+
+    for (i = 0; i < nombre_departements; i++)
+    {
+        printf("Nombre d'étudiants dans le département %s : %d\n", departements[i], compteurs[i]);
+    }
 }
 
 // 3. Afficher les étudiants ayant une moyenne générale supérieure à un certain seuil
 
-void NomberEtudParDepartement(int seuil)
+void EtudiantsAvecMoyenneSuperieure(float seuil)
 {
-	
+    for (i = 0; i < totalEtudiants; i++)
+    {
+        if (et[i].Notegenerale > seuil)
+        {
+            printf("Nom: %s, Prenom: %s,Departement: %s, Note générale: %.2f\n", et[i].Nom, et[i].Prenom,et[i].Departement, et[i].Notegenerale);
+        }
+    }
 }
 
 // 4. Afficher les 3 étudiants ayant les meilleures notes
+void troisMeilleuresNotes() {
+    struct etudiant temp;
 
-// Fonction pour afficher les statistiques
+    for (i = 0; i < totalEtudiants - 1; i++) {
+        for (j = i + 1; j < totalEtudiants; j++) {
+            if (et[i].Notegenerale < et[j].Notegenerale) {
+                temp = et[i];
+                et[i] = et[j];
+                et[j] = temp;
+            }
+        }
+    }
+
+    printf("Les 3 étudiants ayant les meilleures notes :\n");
+    for (i = 0; i < 3 && i < totalEtudiants; i++) {
+        printf("Nom: %s, Prenom: %s, Note générale: %.2f\n", et[i].Nom, et[i].Prenom, et[i].Notegenerale);
+    }
+}
+
+
+// 5. Afficher le nombre d'étudiants ayant réussi dans chaque département
+
+void NombreEtudiantsAyantReussi() {
+    char departements[50][50];
+    int compteurs[50] = {0};
+    int nombre_departements = 0;
+
+    for (i = 0; i < totalEtudiants; i++) {
+        int deja_ajoute = 0;
+        for (j = 0; j < nombre_departements; j++) {
+            if (strcmp(et[i].Departement, departements[j]) == 0) {
+                if (et[i].Notegenerale >= 10) {
+                    compteurs[j]++;
+                }
+                deja_ajoute = 1;
+                break;
+            }
+        }
+
+        if (!deja_ajoute) {
+            strcpy(departements[nombre_departements], et[i].Departement);
+            if (et[i].Notegenerale >= 10) {
+                compteurs[nombre_departements]++;
+            }
+            nombre_departements++;
+        }
+    }
+
+    for (i = 0; i < nombre_departements; i++) {
+        printf("Nombre d'étudiants ayant réussi dans le département %s : %d\n", departements[i], compteurs[i]);
+    }
+}
+
+//========== Menu statistique
 
 void statistique()
 {
-	int choix2;
+	int choix2 ,seuil;
 	
 		do{
 			
@@ -236,6 +375,7 @@ void statistique()
 		printf("3.Afficher les etudiants ayant une moyenne generale superieure a un certain seuil.\n");
 		printf("4.Afficher les 3 etudiants ayant les meilleures notes.\n");
 		printf("5.Afficher le nombre d'etudiants ayant reussi dans chaque departement.\n");
+		printf("6.Quitte.\n");
         printf("que vous choisir : \n");
         scanf("%d",&choix2);
         
@@ -245,16 +385,17 @@ void statistique()
              afficherNombreTotalEtudiants();
             break;
         case 2:
-            
+            NomberEtudParDepartement();
             break;
         case 3:
-        	
+        	printf("Etudiants ayant une moyenne generale superieure a %.2f :\n", seuil);
+        	EtudiantsAvecMoyenneSuperieure(seuil);
             break;
         case 4:
-            
+            troisMeilleuresNotes();
             break;
         case 5:
-        	
+        	NombreEtudiantsAyantReussi();
             break;
         case 6:
         	    return;
@@ -268,9 +409,12 @@ void statistique()
 }
 
 
-//Tri alphabétique des étudiants en fonction de leur nom (de A à Z ou de Z à A).
+//================================== Partie Triage
 
-void TrierparNom(struct etudiant *et, int totalEtudaint) {
+////1. Tri alphabétique des étudiants en fonction de leur nom (de A à Z ou de Z à A).
+//
+
+void TrierparNom(int totalEtudaint) {
 	
     struct etudiant temp;
     
@@ -285,10 +429,10 @@ void TrierparNom(struct etudiant *et, int totalEtudaint) {
     }
 }
 
-//Tri des étudiants par moyenne générale, 
+//2. Tri des étudiants par moyenne générale, 
 //du plus élevé au plus faible ou inversement.
 
-void TrierparNoteGenerale(struct etudiant *et, int totalEtudaint) {
+void TrierparNoteGenerale(int totalEtudaint) {
 	
     struct etudiant temp;
     
@@ -303,14 +447,31 @@ void TrierparNoteGenerale(struct etudiant *et, int totalEtudaint) {
     }
 }
 
-//Tri des étudiants selon leur statut de réussite 
+//3. Tri des étudiants selon leur statut de réussite 
 //(ceux ayant une moyenne supérieure ou égale à 10/20).
 
-//void TrierparRéussite(struct etudiant *et, int totalEtudaint) {
+void TrierparReussite(int totalEtudiant) {
+	
+	struct etudiant temp;
+	
+	for(i = 0 ; i < totalEtudiant-1 ;i++)
+	{
+		for(j = 0 ; j < totalEtudiant-i-1 ; j++)
+		{
+			 if ((et[j].Notegenerale < 10.0 && et[j + 1].Notegenerale >= 10.0) || 
+			 (et[j].Notegenerale >= 10.0 && et[j + 1].Notegenerale >= 10.0 && et[j].Notegenerale < et[j + 1].Notegenerale) ||
+                (et[j].Notegenerale < 10.0 && et[j + 1].Notegenerale < 10.0 && et[j].Notegenerale < et[j + 1].Notegenerale))
+                {
+                temp = et[j];
+                et[j] = et[j + 1];
+                et[j + 1] = temp;
+				}
+		}
+	}
+}
 
-//}
 
-
+//============== Menu Triage
 void trie()
 {
 	int choixtrie;
@@ -325,13 +486,13 @@ void trie()
         
         switch (choixtrie) {
             case 1:
-                TrierparNom(et,totalEtudiants);
+                TrierparNom(totalEtudiants);
                 break;
             case 2:
-                TrierparNoteGenerale(et,totalEtudiants);
+                TrierparNoteGenerale(totalEtudiants);
                 break;
             case 3:
-            	TrierparNom(et,totalEtudiants);
+            	TrierparReussite(totalEtudiants);
                 break;
             case 4:
                 printf("Au revoir !\n");
@@ -344,7 +505,6 @@ void trie()
 	}while(choixtrie != 5);
 }
 
-
 //// ================================ Menu 
 
 int main() {
@@ -352,7 +512,7 @@ int main() {
 
 do{
 
-    printf("========== Menu de gestion des livres: =====================\n\n\n");
+    printf("========== Menu : =====================\n\n\n");
     
     printf("1.Ajouter un etudiant \n");
     printf("2.Afficher les details d'un etudiant \n");
@@ -393,7 +553,7 @@ do{
         	trie();
             break;
         case 9:
-        
+            return;
             break;
         default:
             printf("Choix invalide. Veuillez entrer un numero entre 1 et 6.\n");
@@ -402,3 +562,4 @@ do{
 }while(choix != 9);
     return 0;
 }
+
